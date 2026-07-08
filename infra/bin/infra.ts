9d0dc8db823d6086b8c82cbdfcab68df16cae650
Maxime David <maxday@amazon.com>
2026-07-08 15:37:56 +0000

    feat: deploy codebuild from GitHub

---
+++ infra/bin/infra.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { CodeBuildRunnersStack } from "../lib/codebuild-runners-stack";
+
+const app = new cdk.App();
+new CodeBuildRunnersStack(app, "AwsLambdaCppCodeBuildRunners", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: "eu-west-1",
+  },
+});
---
+++ infra/lib/codebuild-runners-stack.ts
@@ -0,0 +1,61 @@
+import * as cdk from "aws-cdk-lib";
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+
+export class CodeBuildRunnersStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const roleArn = process.env.AWS_CODEBUILD_RELEASE_ROLE_ARN;
+    if (!roleArn) {
+      throw new Error("AWS_CODEBUILD_RELEASE_ROLE_ARN environment variable is required");
+    }
+
+    const codeBuildRole = iam.Role.fromRoleArn(
+      this,
+      "ImportedCodeBuildRole",
+      roleArn
+    );
+
+    const runners = [
+      {
+        name: "aws-lambda-cpp-test-trigger-x86",
+        image: "aws/codebuild/amazonlinux-x86_64-standard:6.0",
+        environmentType: "LINUX_CONTAINER",
+      },
+      {
+        name: "aws-lambda-cpp-test-trigger-arm64",
+        image: "aws/codebuild/amazonlinux-aarch64-standard:3.0",
+        environmentType: "ARM_CONTAINER",
+      },
+    ];
+
+    for (const runner of runners) {
+      new codebuild.Project(this, runner.name, {
+        projectName: runner.name,
+        source: codebuild.Source.gitHub({
+          owner: "maxday",
+          repo: "aws-lambda-cpp",
+          cloneDepth: 1,
+          webhook: true,
+          webhookFilters: [
+            codebuild.FilterGroup.inEventOf(
+              codebuild.EventAction.WORKFLOW_JOB_QUEUED
+            ),
+          ],
+        }),
+        environment: {
+          buildImage:
+            runner.environmentType === "ARM_CONTAINER"
+              ? codebuild.LinuxArmBuildImage.fromCodeBuildImageId(runner.image)
+              : codebuild.LinuxBuildImage.fromCodeBuildImageId(runner.image),
+          computeType: codebuild.ComputeType.MEDIUM,
+        },
+        role: codeBuildRole,
+        timeout: cdk.Duration.minutes(60),
+        queuedTimeout: cdk.Duration.minutes(480),
+      });
+    }
+  }
+}
